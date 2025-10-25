5cc9f08d42cad11b1947bd4fce94f6bb9caadef7
nikhil6392 <nikhilpathak210431@gmail.com>
2025-10-25 17:16:26 +0530

    concurrency using process

---
+++ chapter5/concurrency2.py
@@ -0,0 +1,28 @@
+import concurrent.futures
+import time
+
+def worker(num):
+    print(f'Worker {num} executing')
+    time.sleep(1)
+    return num
+
+if __name__ == '__main__':
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        results = [executor.submit(worker, i) for i in range(5)]
+        for f in concurrent.futures.as_completed(results):
+            print(f.result())
+
+
+
+""" 
+    Output : Worker 0 executing
+             Worker 1 executing
+             Worker 2 executing
+             Worker 3 executing
+             Worker 4 executing
+             0
+             1
+             2
+             3
+             4       
+"""
