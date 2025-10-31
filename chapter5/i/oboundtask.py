7d24571fb75b9d23191c2a5f09173981cf2d8798
nikhil6392 <nikhilpathak210431@gmail.com>
2025-10-31 18:46:50 +0530

    example of i/o bound

---
+++ chapter5/i/oboundtask.py
@@ -0,0 +1,38 @@
+"""I/O Bound tasks"""
+
+
+
+import asyncio
+import aiohttp
+
+
+async def download_coroutine(session, url):
+    async with session.get(url) as response:
+        filename = url.split("/")[-1]
+        with open(filename, "wb") as f:
+            while True:
+                chunk = await response.content.read(1024)
+                if not chunk:
+                    break
+                f.write(chunk)
+        
+        print(f"Downloaded {url}")
+
+
+async def download_all(urls):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for url in urls:
+            task = asyncio.ensure_future(download_coroutine(session, url))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+    
+
+urls = [
+    "https://www.python.org",
+    "https://www.google.com",
+    "https://www.openai.com",
+    "https://www.x.com",
+]
+
+asyncio.run(download_all(urls))
